c98b63bced4310ebd7230913eb26545cad04657e
Aaron <alangley345@gmail.com>
2021-10-27 13:05:57 -0400

    Cleaned up main in cloudfront

--- modules.tf
+++ modules.tf
@@ -1,7 +1,15 @@
+#route53 hosted dns zones and records
 module "public_dns_zones" {
   source = "./public_dns_zones"
 }
 
+#s3 buckets and policies
 module "s3" {
   source = "./s3"
 }
+
+#cloudfront distributions
+module "cloudfront" {
+  source = "./cloudfront"
+}
+
