41397e8da4cc49952f7290ab16d482a20ad331e7
Aaron <alangley345@gmail.com>
2021-10-27 11:49:30 -0400

    Removed some modules, rearranged variables

--- public_dns_zones/aaronlangley.net.tf
+++ public_dns_zones/aaronlangley.net.tf
@@ -3,11 +3,4 @@ resource "aws_route53_zone" "aaronlangley" {
   name = "aaronlangley.net"
 
 }
-data "aws_route53_zone" "aaronlangley" {
-    zone_id = aws_route53_zone.aaronlangley.zone_id
-    depends_on = [aws_route53_zone.aaronlangley]
-}
 
-output "aaronlangley" {
-    value = data.aws_route53_zone.aaronlangley.zone_id
-}
