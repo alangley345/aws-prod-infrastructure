e24160f1eb73a86738dd773f936c559adfe4c2a6
Aaron <alangley345@gmail.com>
2021-11-05 14:45:10 -0400

    moved gw and domain to seperate modules

--- cloudresume_backend/api_gw_domain.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_apigatewayv2_domain_name" "resume" {
-  domain_name = "api.aaronlangley.net"
-
-  domain_name_configuration {
-    certificate_arn = var.gw_crt
-    endpoint_type   = "REGIONAL"
-    security_policy = "TLS_1_2"
-  }
-}
--- modules.tf
+++ modules.tf
@@ -23,3 +23,10 @@ module "cloudresume_backend" {
   gw_cert   = module.certificates.wildcard_arn
   rt53_zone = module.public_dns_zones.aaronlangley_zone_id
 }
+
+module "api_gateway" {
+  source = "./api_gateway"
+
+  gw_cert   = module.certificates.wildcard_arn
+  rt53_zone = module.public_dns_zones.aaronlangley_zone_id
+}
