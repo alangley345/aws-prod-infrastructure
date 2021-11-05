96c4495ab72fc35ba819f0b5a565d47e7d3b8345
Aaron <alangley345@gmail.com>
2021-11-05 11:01:01 -0400

    Added AAAA record for resume.aaronlangley.net

---
+++ cloudresume_frontend/resume.aaronlangley.net_dns.tf
@@ -0,0 +1,23 @@
+resource "aws_route53_record" "resume" {
+  zone_id = var.rt53_zone
+  name    = "resume.aaronlangley.net"
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.resume.domain_name
+    zone_id                = "Z2FDTNDATAQYW2"
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_route53_record" "resume" {
+  zone_id = var.rt53_zone
+  name    = "resume.aaronlangley.net"
+  type    = "AAAA"
+
+  alias {
+    name                   = aws_cloudfront_distribution.resume.domain_name
+    zone_id                = "Z2FDTNDATAQYW2"
+    evaluate_target_health = true
+  }
+}
