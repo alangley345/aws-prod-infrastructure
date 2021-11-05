9be1a412650b55a82ae2acaf13996368f303123d
Aaron <alangley345@gmail.com>
2021-11-05 15:09:05 -0400

    moved gw and domain to seperate modules

--- api_gateway/api_gw_domain.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_apigatewayv2_domain_name" "aaronlangley" {
-  domain_name = "api.aaronlangley.net"
-
-  domain_name_configuration {
-    certificate_arn = var.gw_cert
-    endpoint_type   = "REGIONAL"
-    security_policy = "TLS_1_2"
-  }
-}
