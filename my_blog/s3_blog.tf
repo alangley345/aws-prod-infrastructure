779ebff35ca15c982bb295eea412ec12723e5dbc
Aaron <alangley345@gmail.com>
2021-11-10 15:28:12 -0500

    Added bucket for aaronlangley.net and added it to modules

--- my_blog/s3_blog.tf
+++ my_blog/s3_blog.tf
@@ -2,14 +2,14 @@ resource "aws_s3_bucket" "aaronlangley.net" {
   bucket = "aaronlangley.net"
   acl    = "public-read"
   
-  /*
+
   cors_rule {
     allowed_headers = ["*"]
     allowed_methods = ["GET","HEAD"]
     allowed_origins = ["*"]
     expose_headers  = [""]
   }
-  */
+  
 
   website {
     index_document = "index.html"
