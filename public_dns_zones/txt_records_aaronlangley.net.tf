ad6221b7c6eb996bfbb68bce871d26498c6cdf4f
Aaron Langley <alangley345@gmail.com>
2021-11-13 21:09:17 -0500

    Added records for Github Pages

---
+++ public_dns_zones/txt_records_aaronlangley.net.tf
@@ -0,0 +1,8 @@
+#text record for Github Pages
+resource "aws_route53_record" "github_pages" {
+  zone_id = aws_route53_zone.aaronlangley.zone_id
+  name    = "_github-pages-challenge-alangley345.aaronlangley.net"
+  type    = "TXT"
+  ttl     = "300"
+  records = ["4cd016c1c5c21a9e0418dc68660107"]
+}
