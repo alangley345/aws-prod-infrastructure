bd201b2d419dc837be4c0e3f5bedfab882b9e98e
Aaron <alangley345@gmail.com>
2021-11-18 13:16:25 -0500

    Moved Backend code to it's own repository

--- cloudresume_backend/api_stage_resume.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_apigatewayv2_stage" "resume" {
-  api_id = var.api_id
-  name   = "resume"
-}
--- cloudresume_backend/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "cloudresume_backend_secret" {
-    value = aws_iam_access_key.cloudresume_backend.encrypted_secret
-}
-
-output "cloudresume_backend_ID" {
-    value = aws_iam_access_key.cloudresume_backend.id
-}
