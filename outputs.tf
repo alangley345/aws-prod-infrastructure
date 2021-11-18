d461aa2d3aa21f10ad489dbc75dacdc159265a55
Aaron <alangley345@gmail.com>
2021-11-18 13:19:59 -0500

    Cleaning up this repo

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,3 @@
-output "cloudresume_backend_secret" {
-    value = module.cloudresume_backend.cloudresume_backend_secret
-}
-
-output "cloudresume_backend_ID" {
-    value = module.cloudresume_backend.cloudresume_backend_ID
-}
-
 output "access_token_signing" {
     value = data.local_file.pgp_key
   
