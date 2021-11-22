3124de16ed4df06613d6b72a2e0abb9af0d89f33
Aaron <alangley345@gmail.com>
2021-11-22 15:43:52 -0500

    Added data with outputs about run time account

--- outputs.tf
+++ outputs.tf
@@ -21,3 +21,15 @@ output "wildcard_arn" {
 output "aaronlangley_zone_id" {
   value = module.public_dns_zones.aaronlangley_zone_id
 }
+
+output "account_id" {
+  value = data.aws_caller_identity.current.account_id
+}
+
+output "caller_arn" {
+  value = data.aws_caller_identity.current.arn
+}
+
+output "caller_user" {
+  value = data.aws_caller_identity.current.user_id
+}
