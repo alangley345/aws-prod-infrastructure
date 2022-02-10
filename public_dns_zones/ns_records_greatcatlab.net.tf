d8f7860d702b0fa9d7774b4eca6977fbd0e23693
Aaron Langley <alangley345@gmail.com>
2022-02-09 20:52:58 -0500

    added subdomain

---
+++ public_dns_zones/ns_records_greatcatlab.net.tf
@@ -0,0 +1,7 @@
+resource "aws_route53_record" "tunnel_greatcatlab_net" {
+  zone_id = aws_route53_zone.aaronlangley.zone_id
+  name    = "tunnel.greatcatlab.net"
+  type    = "NS"
+  ttl     = "300"
+  records = ["ns1.he.net","ns2.he.net","ns3.he.net","ns4.he.net","ns5.he.net"]
+}
