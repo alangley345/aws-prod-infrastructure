76dd12cff110b9078c996a092e07db5f067534cf
Aaron <alangley345@gmail.com>
2022-02-11 10:33:07 -0500

    Cleaing up main.tf

--- main.tf
+++ main.tf
@@ -6,8 +6,8 @@ provider "aws" {
 #define location of state file
 terraform {
   backend "s3" {
-    bucket = "myterraformcode"
-    key    = "prod/base_state.tf"
-    region = "us-east-1"
+    bucket   = "myterraformcode"
+    key      = "prod/base_state_tf"
+    region   = "us-east-1"
   }
 }
