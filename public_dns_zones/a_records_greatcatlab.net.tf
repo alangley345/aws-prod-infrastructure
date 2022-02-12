544ee8ec8725990cacfa239d6e05ad37210b2dcb
Aaron Langley <alangley345@gmail.com>
2022-02-12 16:04:46 -0500

    Added  a record

--- public_dns_zones/a_records_greatcatlab.net.tf
+++ public_dns_zones/a_records_greatcatlab.net.tf
@@ -22,11 +22,11 @@ resource "aws_route53_record" "kube03_v4" {
   records = ["207.2.121.163"]
 }
 
-#resource "aws_route53_record" "kube-ep_v4" {
-# zone_id = aws_route53_zone.greatcatlab.zone_id
-# name    = "kube-ep.greatcatlab.net"
-# type    = "A"
-# ttl     = "300"
-# records = ["207.2.121.17"]
-#}
+resource "aws_route53_record" "kube-ep_v4" {
+ zone_id = aws_route53_zone.greatcatlab.zone_id
+ name    = "kube-ep.greatcatlab.net"
+ type    = "A"
+ ttl     = "300"
+ records = ["207.2.121.17"]
+}
 
