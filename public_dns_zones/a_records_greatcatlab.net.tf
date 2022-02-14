c0cc6e21aee83c5e09e39d6ce7a16a1e60fec59a
Aaron <alangley345@gmail.com>
2022-02-14 14:22:12 -0500

    Fixing DNS entries error

--- public_dns_zones/a_records_greatcatlab.net.tf
+++ public_dns_zones/a_records_greatcatlab.net.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "kube01_v4" {
   zone_id = aws_route53_zone.greatcatlab.zone_id
-  name    = "kube01.greatcatlab.net"
+  name    = "kube-01.greatcatlab.net"
   type    = "A"
   ttl     = "300"
   records = ["207.2.120.4"]
@@ -8,7 +8,7 @@ resource "aws_route53_record" "kube01_v4" {
 
 resource "aws_route53_record" "kube02_v4" {
   zone_id = aws_route53_zone.greatcatlab.zone_id
-  name    = "kube02.greatcatlab.net"
+  name    = "kube-02.greatcatlab.net"
   type    = "A"
   ttl     = "300"
   records = ["207.2.121.143"]
@@ -16,7 +16,7 @@ resource "aws_route53_record" "kube02_v4" {
 
 resource "aws_route53_record" "kube03_v4" {
   zone_id = aws_route53_zone.greatcatlab.zone_id
-  name    = "kube03.greatcatlab.net"
+  name    = "kube-03.greatcatlab.net"
   type    = "A"
   ttl     = "300"
   records = ["207.2.121.163"]
