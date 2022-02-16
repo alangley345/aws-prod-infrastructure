f58879b0b22721b429d80583f4bb823c330b59c1
Aaron Langley <alangley345@gmail.com>
2022-02-15 22:12:01 -0500

    Updated DNS

--- public_dns_zones/a_records_greatcatlab.net.tf
+++ public_dns_zones/a_records_greatcatlab.net.tf
@@ -1,22 +1,22 @@
-resource "aws_route53_record" "kube01_v4" {
+resource "aws_route53_record" "cont01_v4" {
   zone_id = aws_route53_zone.greatcatlab.zone_id
-  name    = "kube01.greatcatlab.net"
+  name    = "cont01.greatcatlab.net"
   type    = "A"
   ttl     = "300"
   records = ["207.2.120.4"]
 }
 
-resource "aws_route53_record" "kube02_v4" {
+resource "aws_route53_record" "cont02_v4" {
   zone_id = aws_route53_zone.greatcatlab.zone_id
-  name    = "kube02.greatcatlab.net"
+  name    = "cont02.greatcatlab.net"
   type    = "A"
   ttl     = "300"
   records = ["207.2.121.143"]
 }
 
-resource "aws_route53_record" "kube03_v4" {
+resource "aws_route53_record" "cont03_v4" {
   zone_id = aws_route53_zone.greatcatlab.zone_id
-  name    = "kube03.greatcatlab.net"
+  name    = "cont03.greatcatlab.net"
   type    = "A"
   ttl     = "300"
   records = ["207.2.121.163"]
