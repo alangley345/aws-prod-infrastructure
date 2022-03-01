eb1050648fb1a2e2e57a6911bf6acd37f51d1236
Aaron Langley <alangley345@gmail.com>
2022-02-28 19:36:38 -0500

    updated the cont01.greatcatlab.net ip

--- public_dns_zones/a_records_greatcatlab.net.tf
+++ public_dns_zones/a_records_greatcatlab.net.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "cont01_v4" {
   zone_id = aws_route53_zone.greatcatlab.zone_id
-  name    = "cont01.greatcatlab.net"
+  name    = "relay01.greatcatlab.net"
   type    = "A"
   ttl     = "300"
   records = ["207.2.120.4"]
