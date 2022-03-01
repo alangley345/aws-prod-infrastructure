52669e4d4a7b5a329298e7c1fb02f2ed70cd4c76
Aaron <alangley345@gmail.com>
2022-03-01 15:42:35 -0500

    Added ipa.greatcatlab.net subdomain

--- public_dns_zones/ns_records_greatcatlab.net.tf
+++ public_dns_zones/ns_records_greatcatlab.net.tf
@@ -5,3 +5,11 @@ resource "aws_route53_record" "tunnel_greatcatlab_net" {
   ttl     = "300"
   records = ["ns1.he.net.","ns2.he.net.","ns3.he.net.","ns4.he.net.","ns5.he.net."]
 }
+
+resource "aws_route53_record" "ipa_greatcatlab_net" {
+  zone_id = aws_route53_zone.greatcatlab.zone_id
+  name    = "ipa.greatcatlab.net"
+  type    = "NS"
+  ttl     = "300"
+  records = ["ipa01.greatcatlab.net.","ipa02.greatcatlab.net."]
+}
