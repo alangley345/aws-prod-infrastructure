ca4ecbd67a888bd11bef0aa0224c6e1da0fa60db
Aaron Langley <alangley345@gmail.com>
2022-03-08 06:33:33 -0500

    Updated DNS entries

--- public_dns_zones/a_records_greatcatlab.net.tf
+++ public_dns_zones/a_records_greatcatlab.net.tf
@@ -6,9 +6,9 @@ resource "aws_route53_record" "relay01_v4" {
   records = ["207.2.120.4"]
 }
 
-resource "aws_route53_record" "cont02_v4" {
+resource "aws_route53_record" "relay02_v4" {
   zone_id = aws_route53_zone.greatcatlab.zone_id
-  name    = "cont02.greatcatlab.net"
+  name    = "relay02.greatcatlab.net"
   type    = "A"
   ttl     = "300"
   records = ["207.2.121.143"]
