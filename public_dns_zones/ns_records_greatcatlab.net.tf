ea10113ae1d959a9012d94528341d0f18f5d4248
Aaron Langley <alangley@gmail.com>
2022-03-15 11:07:47 -0400

    Removed NS records for int.greatcatlab.net

--- public_dns_zones/ns_records_greatcatlab.net.tf
+++ public_dns_zones/ns_records_greatcatlab.net.tf
@@ -5,11 +5,3 @@ resource "aws_route53_record" "tunnel_greatcatlab_net" {
   ttl     = "300"
   records = ["ns1.he.net.","ns2.he.net.","ns3.he.net.","ns4.he.net.","ns5.he.net."]
 }
-
-resource "aws_route53_record" "int_greatcatlab_net" {
-  zone_id = aws_route53_zone.greatcatlab.zone_id
-  name    = "int.greatcatlab.net"
-  type    = "NS"
-  ttl     = "300"
-  records = ["ipa01.int.greatcatlab.net.","ipa02.int.greatcatlab.net."]
-}
