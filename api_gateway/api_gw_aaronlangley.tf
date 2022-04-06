97fcf94841e1c5a40e5d3348d4313369068d149d
Aaron Langley <aaron@thinkpad.int.greatcatlab.net>
2022-04-05 21:11:38 -0400

    added CORS for api.aaronlangley.net

--- api_gateway/api_gw_aaronlangley.tf
+++ api_gateway/api_gw_aaronlangley.tf
@@ -2,6 +2,12 @@ resource "aws_apigatewayv2_api" "my_domain" {
   name          = "aaronlangley.net-api-gw"
   protocol_type = "HTTP"
   description   = "Backend to aaronlangley.net"
+  cors_configuration {
+    allow_origins = ["*"]
+    allow_methods = ["PUT", "GET"]
+    allow_headers = ["application/json"]
+    max_age = 300
+  }
   
   tags          = {
     Environment = "production"
