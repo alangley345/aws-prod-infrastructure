3b4c338ebb72ccbcf27cf63170a37155d67b716a
Aaron Langley <aaron@thinkpad.int.greatcatlab.net>
2022-04-07 07:34:34 -0400

    Added expose headers definition for CORS

--- api_gateway/api_gw_aaronlangley.tf
+++ api_gateway/api_gw_aaronlangley.tf
@@ -3,9 +3,10 @@ resource "aws_apigatewayv2_api" "my_domain" {
   protocol_type = "HTTP"
   description   = "Backend to aaronlangley.net"
   cors_configuration {
-    allow_origins = ["*"]
-    allow_methods = ["*"]
-    allow_headers = ["*"]
+    allow_origins  = ["*"]
+    allow_methods  = ["*"]
+    allow_headers  = ["*"]
+    expose_headers = ["*"]
     max_age = 300
   }
   
