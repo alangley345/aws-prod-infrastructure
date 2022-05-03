1953dd7a26944bd93b7e4cf5bfc1f28e1393d45f
Aaron Langley <alangley@gmail.com>
2022-05-02 22:21:32 -0400

    Updated dns for ovh host

--- public_dns_zones/a_records_greatcatlab.net.tf
+++ public_dns_zones/a_records_greatcatlab.net.tf
@@ -33,7 +33,7 @@ resource "aws_route53_record" "vdi01" {
 #OVH Server
 resource "aws_route53_record" "ovh_main_v4" {
  zone_id = aws_route53_zone.greatcatlab.zone_id
- name    = "proxmox.greatcatlab.net"
+ name    = "dockerhost01.greatcatlab.net"
  type    = "A"
  ttl     = "300"
  records = ["147.135.5.154"]
--- public_dns_zones/aaaa_records_greatcatlab.net.tf
+++ public_dns_zones/aaaa_records_greatcatlab.net.tf
@@ -1,26 +1,8 @@
 #OVH Server
 resource "aws_route53_record" "ovh_main_v6" {
  zone_id = aws_route53_zone.greatcatlab.zone_id
- name    = "proxmox.greatcatlab.net"
+ name    = "dockerhost01.greatcatlab.net"
  type    = "AAAA"
  ttl     = "300"
  records = ["2604:2dc0:100:59a::"]
-}
-
-#IPA Server 01
-resource "aws_route53_record" "ipa01_v6" {
- zone_id = aws_route53_zone.greatcatlab.zone_id
- name    = "ipa01.int.greatcatlab.net"
- type    = "AAAA"
- ttl     = "300"
- records = ["2001:470:e107:1010::aaaa"]
-}
-
-#IPA Server 02
-resource "aws_route53_record" "ipa02_v6" {
- zone_id = aws_route53_zone.greatcatlab.zone_id
- name    = "ipa02.int.greatcatlab.net"
- type    = "AAAA"
- ttl     = "300"
- records = ["2001:470:e107:1010::bbbb"]
 }
