abeb79eec300f1e56a4922ab6e039c7b7a25a3a8
Aaron Langley <alangley345@gmail.com>
2022-05-19 22:36:46 -0400

    Added portainer subdomain

--- public_dns_zones/cname_records_greatcatlab.net.tf
+++ public_dns_zones/cname_records_greatcatlab.net.tf
@@ -6,3 +6,12 @@ resource "aws_route53_record" "nas" {
  ttl     = "300"
  records = ["1.tunnel.greatcatlab.net"]
 }
+
+#OVH Server
+resource "aws_route53_record" "portainer" {
+ zone_id = aws_route53_zone.greatcatlab.zone_id
+ name    = "portainer.greatcatlab.net"
+ type    = "CNAME"
+ ttl     = "300"
+ records = ["dockerhost01.greatcatlab.net"]
+}
