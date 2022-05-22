fe3ed6a21d95b2767f1c4e8c7608c5a486467476
Aaron Langley <alangley345@gmail.com>
2022-05-22 09:07:01 -0400

    added record for cockpit

--- public_dns_zones/cname_records_greatcatlab.net.tf
+++ public_dns_zones/cname_records_greatcatlab.net.tf
@@ -1,4 +1,4 @@
-#OVH Server
+#My Synology NAS
 resource "aws_route53_record" "nas" {
  zone_id = aws_route53_zone.greatcatlab.zone_id
  name    = "nas.greatcatlab.net"
@@ -11,6 +11,15 @@ resource "aws_route53_record" "nas" {
 resource "aws_route53_record" "portainer" {
  zone_id = aws_route53_zone.greatcatlab.zone_id
  name    = "portainer.greatcatlab.net"
+ type    = "CNAME"
+ ttl     = "300"
+ records = ["dockerhost01.greatcatlab.net"]
+}
+
+#OVH Server
+resource "aws_route53_record" "portainer" {
+ zone_id = aws_route53_zone.greatcatlab.zone_id
+ name    = "cockpit.greatcatlab.net"
  type    = "CNAME"
  ttl     = "300"
  records = ["dockerhost01.greatcatlab.net"]
