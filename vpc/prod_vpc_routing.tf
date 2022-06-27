c86b6041c757a9f274ced21029b9dea464a2acba
Aaron Langley <alangley@gmail.com>
2022-06-27 11:58:56 -0400

    Added VPC for generic production resources

--- vpc/prod_vpc_routing.tf
+++ vpc/prod_vpc_routing.tf
@@ -1,4 +1,4 @@
-#Internet gateway for Fortune Cookie VPC
+#Internet gateway for Production VPC
 resource "aws_internet_gateway" "production" {
   vpc_id     = aws_vpc.production.id
   depends_on = [aws_vpc.production]
@@ -7,7 +7,7 @@ resource "aws_internet_gateway" "production" {
   }
 }
 
-#create route table for VPC
+#create route table for Production VPC
 resource "aws_route_table" "production" {
   vpc_id     = aws_vpc.production.id
   depends_on = [aws_internet_gateway.production, aws_vpc.production]
