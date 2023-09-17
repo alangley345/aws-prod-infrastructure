95e8dfefb791a7c14db2295217db4bbe57912db8
Aaron Langley <alangley345@gmail.com>
2023-09-17 06:25:34 -0400

    Update a_records_greatcatlab.net.tf
    Commit out old vultr record, manually removing it in anticipation of migrating DNS.

--- public_dns_zones/a_records_greatcatlab.net.tf
+++ public_dns_zones/a_records_greatcatlab.net.tf
@@ -1,8 +1,8 @@
 #vultr
-resource "aws_route53_record" "vultr_01_v4" {
- zone_id = aws_route53_zone.greatcatlab.zone_id
- name    = "vultr01.greatcatlab.net"
- type    = "A"
- ttl     = "300"
- records = ["104.238.131.32"]
-}
+#resource "aws_route53_record" "vultr_01_v4" {
+#zone_id = aws_route53_zone.greatcatlab.zone_id
+#name    = "vultr01.greatcatlab.net"
+#type    = "A"
+#ttl     = "300"
+#records = ["104.238.131.32"]
+#}
